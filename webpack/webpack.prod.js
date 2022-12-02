07f1375f60605220dce2c7cc4db7dd9849f11dc7
Suhas S Murthy <murthy.suhas@gmail.com>
2022-12-03 00:28:57 +0530

    initial setup with husky

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    // Define env variables through webpack
+    // new webpack.DefinePlugin({
+    //   'process.env.name': JSON.stringify('Suhas Murthy'),
+    // }),
+  ],
+};
