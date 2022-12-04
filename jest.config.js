680f3e856f51a15f7c6b409c8edb6ad1fc3c63b9
Suhas S Murthy <murthy.suhas@gmail.com>
2022-12-04 22:58:04 +0530

    docker setup in progress

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import './styles.css'
+import './styles.css'
 
 export const App = () => {
   return <h1>Starter code</h1>
