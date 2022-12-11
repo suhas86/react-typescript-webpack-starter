33f1cb01f8347f9ea7f068e2b3575b278f1e7d16
Suhas S Murthy <murthy.suhas@gmail.com>
2022-12-11 13:08:32 +0530

    docker dev env setup

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,7 @@ module.exports = {
     '\\.[jt]sx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(css|less|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy',
   },
 }
